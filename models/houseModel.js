84fac7bc6bb73d1968a8f90c953ffc0fe509cc79
Koteswaramma Bekkam <bekkamkoteswaramma@gmail.com>
2026-09-25 18:49:55 +0530

    Add files via upload

---
+++ models/houseModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const houseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  rent: { type: Number, required: true },
+  location: { type: String, required: true }, // "Amaravati, AP"
+  image: String, // image URL
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  ownerName: String,
+  ownerPhone: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('House', houseSchema);
